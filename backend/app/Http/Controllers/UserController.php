f1ebccbf1c042ac5ac8ff51e90e12a4abbacc446
shizokura <edwardguevarra2003@gmail.com>
2023-10-09 20:50:18 +0800

    Login and Register

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -5,15 +5,20 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
 
 class UserController extends Controller
 {
     public function login(Request $request)
     {
-        $request->validate([
-            'email' => 'required|email',
-            'password' => 'required'
-        ]);
+        try {
+            $request->validate([
+                'email' => 'required|email',
+                'password' => 'required'
+            ]);
+        } catch (ValidationException $e) {
+            return response()->json([ 'status' => 'error', 'message' => 'Validation Error', 'messages' => $e->errors() ], 503);
+        }
 
         $user = User::where('email', $request->email)->first();
 
@@ -22,5 +27,24 @@ class UserController extends Controller
         }
 
         return response()->json([ 'status' => 'success', 'message' => 'Successful Login', 'token' => $user->createToken('user')->plainTextToken ], 200);
+    }
+
+    public function register(Request $request)
+    {
+        try {
+            $request->validate([
+                'email' => 'required|email|unique:users',
+                'password' => 'required'
+            ]);
+        } catch (ValidationException $e) {
+            return response()->json([ 'status' => 'error', 'message' => 'Validation Error', 'messages' => $e->errors() ], 503);
+        }
+
+        User::create([
+            'email' => $request->email,
+            'password' => $request->password
+        ]);
+
+        return response()->json([ 'status' => 'success', 'message' => 'Successful Registered' ], 200);
     }
 }
